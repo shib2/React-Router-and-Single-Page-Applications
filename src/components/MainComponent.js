df128043156da4ad87b27483e2c874eb94039316
unknown <392595061@qq.com>
2020-04-24 18:19:00 -0700

    A2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
-import DishDetail from './DishDetailComponent'
+import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -52,6 +53,7 @@ render(){
     <Header />
     <Switch>
       <Route path = "/home" component = { HomePage } />
+      <Route path = "/aboutus" component = { () =><About leaders = {this.state.leaders}/> } />
       <Route exact path = "/menu" component = { () => <Menu dishes = { this.state.dishes} />} />
       <Route path = "/menu/:dishId" component={DishWithId} />
       <Route exact path='/contactus' component={Contact} />} />
